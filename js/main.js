c3f9c73fa0b40b638bdedba6ab4d8f049be87611
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2022-01-03 15:52:26 +0100

    autre essai

--- js/main.js
+++ js/main.js
@@ -10,3 +10,50 @@ function parse_args() {
 
     return $_GET;
 }
+
+
+function GPXParser(xmlDoc) {
+    this.xmlDoc = xmlDoc;
+}
+
+GPXParser.prototype.getTrackSegment = function(trackSegmentElement) {
+    var trackpointElements = trackSegmentElement.getElementsByTagName("trkpt");
+    var trackPoints = [];
+
+    for(var i = 0; i < trackpointElements.length; i++) {
+        var trackpointElement = trackpointElements[i];
+        var lng = parseFloat(trackpointElement.getAttribute("lon"));
+        var lat = parseFloat(trackpointElement.getAttribute("lat"));
+        var point = {lat: lat, lng: lng};
+
+        var elevationElements = trackpointElement.getElementsByTagName("ele");
+        if (elevationElements.length > 0) {
+            point.elevation = parseFloat(elevationElements[0].childNodes[0].nodeValue);
+        }
+        trackPoints.push(point);
+    }
+
+    return {trackPoints: trackPoints};
+}
+
+GPXParser.prototype.getTrack = function(trackElement) {
+    var segmentElements = trackElement.getElementsByTagName("trkseg");
+    var segments = [];
+    for(var i = 0; i < segmentElements.length; i++) {
+        var segment = this.getTrackSegment(segmentElements[i]);
+        segments.push(segment);
+    }
+    return {segments: segments };
+}
+
+
+GPXParser.prototype.getTracks = function() {
+    var trackElements = this.xmlDoc.documentElement.getElementsByTagName("trk");
+    var tracks = [];
+    for(var i = 0; i < trackElements.length; i++) {
+        var track = this.getTrack(trackElements[i]);
+        tracks.push(track);
+    }
+    return tracks;
+}
+
