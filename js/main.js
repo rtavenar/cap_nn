31d8af10b29adf0fc0c7b9fce9cce4185a494f71
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2022-01-04 09:19:48 +0100

    Bugfix

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function nearest_point_in_track(lat, lon, array_of_points) {
     var arg_min = -1;
     for (var i=0; i<array_of_points.length; ++i) {
         var p = array_of_points[i];
-        var dist = (p.lat - lat) ** 2 + (p.lon - lon) ** 2;
+        var dist = (p["lat"] - lat) ** 2 + (p["lon"] - lon) ** 2;
         if (dist < min_dist) {
             min_dist = dist;
             arg_min = i;
