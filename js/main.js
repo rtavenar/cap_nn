e78ac9821c4975de7a61bef7e039edf23b714022
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2022-01-04 09:15:44 +0100

    distance covered

--- js/main.js
+++ js/main.js
@@ -10,3 +10,21 @@ function parse_args() {
 
     return $_GET;
 }
+
+function nearest_point_in_track(lat, lon, array_of_points) {
+    var min_dist = Infinity;
+    var arg_min = -1;
+    for (var i=0; i<array_of_points.length; ++i) {
+        var p = array_of_points[i];
+        var dist = (p.lat - lat) ** 2 + (p.lon - lon) ** 2;
+        if (dist < min_dist) {
+            min_dist = dist;
+            arg_min = i;
+        }
+    }
+    return i;
+}
+
+function distance_covered(lat, lon, gpx_o) {
+    return gpx_o.distance["cumul"][nearest_point_in_track(lat, lon, gpx_o["points"])];
+}
