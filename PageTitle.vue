d48f7d55a97200a9a35f7e8154ba45ab8c7b7fa3
Rémi Emonet <remi.emonet@reverse--com.heeere>
2022-01-08 00:37:39 +0100

    Set tab title based on gpx info

---
+++ PageTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <span style="display: none">{{ v }}</span>
+</template>
+<script>
+export default Vue.defineComponent({
+  props: ["v"],
+  mounted() {
+    this.setTitle();
+  },
+  methods: {
+    setTitle() {
+      window.document.title = this.v;
+    },
+  },
+  watch: {
+    v(v) {
+      this.setTitle();
+    },
+  },
+});
+</script>
